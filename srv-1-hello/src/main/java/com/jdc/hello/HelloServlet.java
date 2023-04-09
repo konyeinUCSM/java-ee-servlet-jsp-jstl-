c5f90f8205b43ee6705d1c449e66d160f020254f
konyeinUCSM <aungkonyein548@gmail.com>
2023-04-09 21:22:07 +0630

    hello servlet

---
+++ srv-1-hello/src/main/java/com/jdc/hello/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.jdc.hello;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.getWriter().append("<h1>")
+		.append("Hello Servlet").append("</h1>").flush();
+	}
+}
